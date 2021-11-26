15ef126c39334e52118d1610d11c0287ac5aa21a
Bauman Robotics | BMSTU <baumanrobotics@gmail.com>
2021-11-26 22:52:28 +0300

    .

---
+++ MDK-ARM/RTE/_simple_gen/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'simple_gen' 
+ * Target:  'simple_gen' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
